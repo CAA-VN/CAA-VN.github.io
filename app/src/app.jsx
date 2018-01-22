e0812ad8e05fbf74fb226392af0adc1fd018d857
Starr Yang <starrtyang@berkeley.edu>
2018-01-21 23:10:08 -0800

    Moved index to root (and updated image links)

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -19,10 +19,10 @@ class App extends React.Component{
                     <p>Created by a team of students, the video game explores the gritty transition from high school to college. Every step forward feels like five steps back, and something framed as a utopia quickly becomes a nightmare. This is the darker world that pamphlets won’t show you. Unfortunately, it’s also the more common one.</p>
                     <p>In the game, you will meet students who represent ideologies that a person may experience. From freshmen beginning college alongside you, to upperclassmen who have yet to plan their next steps, to people who seem too certain about themselves, to a transfer student who challenges the very notion of truth, these are the friends you face your challenges with.</p>
                 </div>
-                <img src="images/vi.jpg"/>
+                <img src="app/images/vi.jpg"/>
                 </section>
                 <section className="info static-greyer">
-                <img src="images/rosa.png"/>
+                <img src="app/images/rosa.png"/>
                 <div className="info-text-left">
                     <h2>Support us on Patreon!</h2>
                     <p>We're just a bunch of volunteer undergrad students, any contributions would be greatly appreciated!</p>
@@ -33,7 +33,7 @@ class App extends React.Component{
                     <h2>Follow us on social media</h2>
                     <p>Help us get the word out!</p>
                 </div>
-                <img src="images/soda.png"/>
+                <img src="app/images/soda.png"/>
                 </section>
             </main>
     )
